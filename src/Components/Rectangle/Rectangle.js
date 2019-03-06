e950f05d9ab7edcebbba0065a60e78fca7d3f9fa
Tirunagari-Harika <36506029+Tirunagari-Harika@users.noreply.github.com>
2019-03-06 18:32:41 +0800

    Update Rectangle.js

--- src/Components/Rectangle/Rectangle.js
+++ src/Components/Rectangle/Rectangle.js
@@ -1,87 +1,79 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import "./Rectangle.css";
+import Tooltip from '@material-ui/core/Tooltip';
 
-class Rectangle extends Component{    
-    backgrounds = {};
+const StyleWrapper = styled.div`
+    
+    transform:rotateY(180deg);   
 
-    constructor(props){
-        super(props);        
-        this.backgrounds = {
-            selectBackground: this.generateLinearGradient(90,this.props.selectedColor,100,this.props.emptyColor,0),
-            emptyBackground: this.generateLinearGradient(90,this.props.selectedColor,0,
-                this.props.emptyColor,0)
-        };
-        this.state = {
-            maxRating:parseInt(this.props.maxRating),
-            currentRating:parseInt(this.props.currentRating),           
-        }
+    .rectangle:hover, .rectangle:hover ~ .rectangle{        
+        background-image: ${props => 
+            "linear-gradient(90deg," + props.selectedColor + " 100%," + props.emptyColor + " 0%)"}        
     }
 
-    markRate = (id) => {
+    .ratingClass{
+        background-image: ${props => 
+                    "linear-gradient(90deg," + props.selectedColor + " 100%," + props.emptyColor + " 0%)"}
+    }
+
+    .emptyClass{
+        background-image: ${props => 
+                    "linear-gradient(90deg," + props.selectedColor + " 0%," + props.emptyColor + " 0%)"}
+    }
+   
+`
+
+class Rectangle extends Component{
+    state = {
+        maxRating:parseInt(this.props.maxRating),
+        currentRating:parseInt(this.props.currentRating),           
+    }   
+
+    markRate = (id) => {       
         this.setState({currentRating:parseInt(id)},() => {
-           console.log("Current ",this.state.currentRating);
+            console.log("Current ",this.state.currentRating);
             this.props.updateRating(this.state.currentRating);
         });
-    }
+    }    
 
-    generateLinearGradient = (angle,color1,percent1,color2,percent2) => {
-        let backgroundStyle = [];
-        backgroundStyle.push(angle + "deg");
-        backgroundStyle.push(color1 + " " + percent1 +"%");
-        backgroundStyle.push(color2 + " " + percent2 + "%");
-
-        let b = "linear-gradient(" + [...backgroundStyle].join(",") + ")";
-        return b;
-    }
-
-    addRate = (id) => {      
-        if(id <= this.state.currentRating){ 
-            return {backgroundImage:this.backgrounds.selectBackground};       
+    addRate = (id) => {        
+        if(id <= this.state.currentRating){           
+            return "ratingClass";
         }
-        else if(id > this.state.currentRating){  
-            return {backgroundImage:this.backgrounds.emptyBackground};                
-        }       
-    }
-
-    selectionEnter = (id) => {        
-        for(let i=1; i<=id; i++){
-            ReactDOM.findDOMNode(this.refs[i]).style.backgroundImage =  this.backgrounds.selectBackground;
-        }       
-    }
-
-    selectionLeave = (id) => {
-        if(this.state.currentRating){
-            for(let i=1; i<=this.state.currentRating; i++){              
-                ReactDOM.findDOMNode(this.refs[i]).style.backgroundImage = this.backgrounds.selectBackground;
-            }
-        }        
+        else if(id > this.state.currentRating){           
+            return "emptyClass";           
+        }   
         
-        for(let j=this.state.currentRating+1; j<=this.state.maxRating; j++){           
-            ReactDOM.findDOMNode(this.refs[j]).style.backgroundImage = this.backgrounds.emptyBackground;
-        }     
-       
     }
 
+    
     createRating = () => {
         let arr = [];
-        for(let i=0; i<this.state.maxRating; i++){
-            let addRate = this.addRate(i+1);
-            arr.push(<div className="rectangle" id={i+1} key={i} 
-                ref={i+1}
-                style={addRate}
-                onMouseEnter = {(event) => this.selectionEnter(event.currentTarget.id)}
-                onMouseLeave = {(event) => this.selectionLeave(event.currentTarget.id)}
-                onClick={(event) => this.markRate(event.currentTarget.id)}>
-            </div>)
+        
+        for(let i=this.state.maxRating; i>=1; i--){
+            let styleClass = ["rectangle"];
+            let addRate = this.addRate(i);
+            styleClass.push(addRate);
+            
+            arr.push(<Tooltip title={i} placement="top" key={i-1}>
+                <div className={[...styleClass].join(" ")} 
+                    id={i}                                            
+                    onClick={(event) => this.markRate(event.currentTarget.id)}>
+                </div>
+             </Tooltip>)
         }
         return arr;
     }
 
     render(){
         return (<div>
-         {this.createRating()}             
+         <StyleWrapper selectedColor={this.props.selectedColor}
+                emptyColor={this.props.emptyColor}>
+                {this.createRating()}
+        </StyleWrapper>  
+            
         </div>)
     }
 }
